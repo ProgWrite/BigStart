c4727820b9e033a21fcec7c4a924efa28bf9c48a
ProgWrite <dimkakuz123@gmail.com>
2025-01-11 21:35:08 +0300

    change smth

--- Practice/Dif_Task.java
+++ Practice/Dif_Task.java
@@ -2,10 +2,10 @@
 public class Dif_Task {
     public static void main(String[] args) {
         System.out.println("Я проверю как работает гитхаб");
-
         int a = 10;
         int b = 20;
-        int sum = a + b;
+        int ab = a * b;
+        int sum = a + b + b;
         System.out.println(sum);
     }
 }
